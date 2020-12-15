90d7cade4111b83e0794d61080c4597e53df5927
Edvard <edvard.hedlund@gmail.com>
2020-12-15 17:19:26 +0100

    update2

--- components/InfoForm.js
+++ components/InfoForm.js
@@ -87,11 +87,26 @@ export default function InfoForm() {
         return validPatterns.some((pattern) => pattern.test(phone)) ? isEmail(emailValue) : Alert.alert('Phonenumber is not valid')
     }
 
-    //checks if the email is valid, on a basic level.
+    //checks if the email is valid using regular expressions
     function isEmail(val) {
+        const validPatterns = [
+            /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+        ]
+        return validPatterns.some((pattern) => pattern.test(val)) ? isSuccess() : Alert.alert('Email is not valid');
+        /*
         const ai = val.indexOf("@");
         const gdi = val.split("").reduce((acc, char, i) => char === "." ? i : acc, 0);
         return ai > -1 && gdi > ai ? isSuccess() : Alert.alert('Email is not valid');
+        */
+    }
+
+    //checks if email is valid using the Mailadress class
+    function isValidEmail(val){
+        try {
+            address = new MailAddress(address).Address;
+        } catch(FormatException) {
+            // address is invalid
+        }
     }
 
     function isSuccess() {
